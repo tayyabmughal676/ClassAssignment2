a3cb067ec4c4731791cceb253b119619177cc833
Tayyabmughal676 <Tayyabmughal676@gmail.com>
2020-12-03 14:28:13 +0500

    A lot of updates.

--- app/src/main/java/com/example/mylistview/SpinnerActivity.java
+++ app/src/main/java/com/example/mylistview/SpinnerActivity.java
@@ -21,6 +21,7 @@ public class SpinnerActivity extends AppCompatActivity implements AdapterView.On
             "Thunder"
     };
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
